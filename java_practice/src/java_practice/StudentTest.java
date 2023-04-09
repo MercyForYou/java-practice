410915e4a06fc3eaf726275095fd97424a4fb2d8
onasis08 <onasis08@naver.com>
2023-04-09 21:17:25 +0900

    chapter 5

---
+++ java_practice/src/java_practice/StudentTest.java
@@ -0,0 +1,10 @@
+package java_practice;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		StudentPractice studentHong = new StudentPractice();
+		studentHong.setStudentName("Hong");
+		
+		System.out.println(studentHong.getStudentName());
+	}
+}
