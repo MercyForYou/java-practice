fee2ead7afe27ea672086e4510ebb5f00712e55e
onasis08 <onasis08@naver.com>
2023-04-12 14:11:29 +0900

    .

---
+++ java_practice/src/java_practice/Person2.java
@@ -0,0 +1,19 @@
+package java_practice;
+
+public class Person2 {
+	String name;
+	float height;
+	float weight;
+	
+	public Person2() {}
+	
+	public Person2(String pname) {
+		name = pname;
+	}
+
+	public Person2(String pname, float pheight, float pweight) {
+		name = pname;
+		height = pheight;
+		weight = pweight;
+	}
+}
